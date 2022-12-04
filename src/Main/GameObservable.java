c715ffda7d7cad3382e518ffc086511642fa3d70
Martin <martin409pd@gmail.com>
2022-12-04 17:49:42 +0100

    GameObservable update

--- src/Main/GameObservable.java
+++ src/Main/GameObservable.java
@@ -2,26 +2,49 @@ package Main;
 
 import DataType.GameState;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class GameObservable {
 
 
-    void add(){
+    private List<GameObserver> gameObservers;
+    private List<Integer> players;
 
-
+    public GameObservable(){
+        gameObservers = new ArrayList<>();
+        players = new ArrayList<>();
     }
 
+    void add(GameObserver observer){
+        gameObservers.add(observer);
+    }
+
+
+
     void addPlayer(int playerIdx, GameObserver observer){
-
-
+        if(players.size()<5){
+            players.add(playerIdx);
+            gameObservers.add(observer);
+        }
     }
 
 
     void remove(GameObserver observer){
-
-
+        try {
+            gameObservers.remove(observer);
+        }
+        catch (Exception e){
+            System.err.println("observer "+observer.toString()+"don't exist");
+        }
     }
 
+
     void notifyAll(GameState message){
+
+        for (GameObserver gameObserver : gameObservers) {
+            gameObserver.notify(String.valueOf(message));
+        }
 
     }
 
