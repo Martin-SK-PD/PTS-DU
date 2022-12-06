b1a71599d3a4f943b697f7e8badedb632b4b9808
Martin <martin409pd@gmail.com>
2022-12-06 17:51:22 +0100

    SleepingQueens update

--- src/Main/SleepingQueens.java
+++ src/Main/SleepingQueens.java
@@ -4,23 +4,54 @@ import DataType.Position.Position;
 import DataType.Position.SleepingQueenPosition;
 import DataType.Queen;
 
-import java.util.Map;
-import java.util.Optional;
+import java.util.*;
 
 public class SleepingQueens extends QueenCollection{
 
 
     private Map<Position, Queen> sleepingQueens;
 
+    public SleepingQueens(){
+        sleepingQueens = new HashMap<>();
+
+
+        List<Integer> points = new ArrayList<>();
+        for (int i = 0; i < 4; i++ ) {
+            points.add(5);
+        }
+        for (int i = 0; i < 4; i++ ) {
+            points.add(10);
+        }
+        for (int i = 0; i < 3; i++ ) {
+            points.add(15);
+        }
+        points.add(20);
+
+        Collections.shuffle(points);
+
+        for (int i = 0; i < 12; i++) {
+            addQueen(new Queen(points.get(i)));
+        }
+    }
 
     @Override
     public void addQueen(Queen queen){
-
+        sleepingQueens.put(new SleepingQueenPosition(sleepingQueens.size()),queen);
     }
 
     @Override
     public Optional<Queen> removeQueen(SleepingQueenPosition position){
-        return Optional.empty();
+        Optional<Queen> queen = Optional.empty();
+
+
+        for (Position position1 :sleepingQueens.keySet()){
+            if(position1.getCardIndex() == position.getCardIndex()){
+                queen = Optional.of(sleepingQueens.get(position1));
+                sleepingQueens.remove(position1,queen.get());
+                break;
+            }
+        }
+        return queen;
     }
 
 
